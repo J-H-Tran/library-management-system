ff45dc566218bf134c0a884c46d970d5cbd219c9
Tran, Jonathan | INPD <ts-jonathan.tran@rakuten.com>
2024-08-21 13:30:37 +0900

    added methods to util class

--- src/main/java/com/library/util/FileIOHelper.java
+++ src/main/java/com/library/util/FileIOHelper.java
@@ -1,11 +1,35 @@
 package com.library.util;
 
+import com.library.dao.DatabaseHelper;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class FileIOHelper {
+    private final DatabaseHelper dbHelper;
+
+    public FileIOHelper() {
+        dbHelper = new DatabaseHelper();
+    }
     public void backupDatabase(String filePath) {
-        // code to back up database to file
+        String sql = "BACKUP TO '" + filePath + "'";
+        try (Connection conn = dbHelper.connect();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Database backup completed successfully.");
+        } catch (SQLException e) {
+            Logger.log("An error occurred while backing up the database.");
+        }
     }
 
     public void restoreDatabase(String filePath) {
-        // code to restore database from file
+        String sql = "RESTORE FROM '" + filePath + "'";
+        try (Connection conn = dbHelper.connect();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Database restore completed successfully.");
+        } catch (SQLException e) {
+            Logger.log("An error occurred while restoring the database.");
+        }
     }
 }
