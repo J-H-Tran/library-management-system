35c1c3285edc0872d3a0e2fbde8b95b483b69a00
Tran, Jonathan | INPD <ts-jonathan.tran@rakuten.com>
2024-08-21 17:41:53 +0900

    updated model names to be consistent with database naming

--- src/main/java/com/library/service/LibraryManager.java
+++ src/main/java/com/library/service/LibraryManager.java
@@ -1,20 +1,20 @@
 package com.library.service;
 
-import com.library.dao.BookDAO;
-import com.library.dao.MemberDAO;
-import com.library.dao.TransactionDAO;
+import com.library.dao.BooksDAO;
+import com.library.dao.MembersDAO;
+import com.library.dao.TransactionsDAO;
 import com.library.dao.DatabaseHelper;
 import java.sql.Date;
 
 public class LibraryManager {
-    private final BookDAO bookDAO;
-    private final MemberDAO memberDAO;
-    private final TransactionDAO transactionDAO;
+    private final BooksDAO booksDAO;
+    private final MembersDAO membersDAO;
+    private final TransactionsDAO transactionsDAO;
 
     public LibraryManager() {
-        bookDAO = new BookDAO();
-        memberDAO = new MemberDAO();
-        transactionDAO = new TransactionDAO();
+        booksDAO = new BooksDAO();
+        membersDAO = new MembersDAO();
+        transactionsDAO = new TransactionsDAO();
 
         DatabaseHelper dbHelper = new DatabaseHelper();
 
@@ -24,49 +24,49 @@ public class LibraryManager {
 
     // Book CRUD operations
     public void addBook(String title, String author, String isbn, int availableCopies) {
-        bookDAO.addBook(title, author, isbn, availableCopies);
+        booksDAO.addBook(title, author, isbn, availableCopies);
     }
 
     public void getBook(int id) {
-        bookDAO.getBook(id);
+        booksDAO.getBook(id);
     }
 
     public void updateBook(int id, String title, String author, String isbn, int availableCopies) {
-        bookDAO.updateBook(id, title, author, isbn, availableCopies);
+        booksDAO.updateBook(id, title, author, isbn, availableCopies);
     }
 
     public void deleteBook(int id) {
-        bookDAO.deleteBook(id);
+        booksDAO.deleteBook(id);
     }
 
     // Member CRUD operations
     public void addMember(String name, String email, String phone) {
-        memberDAO.addMember(name, email, phone);
+        membersDAO.addMember(name, email, phone);
     }
 
     public void getMember(int id) {
-        memberDAO.getMember(id);
+        membersDAO.getMember(id);
     }
 
     public void updateMember(int id, String name, String email, String phone) {
-        memberDAO.updateMember(id, name, email, phone);
+        membersDAO.updateMember(id, name, email, phone);
     }
 
     public void deleteMember(int id) {
-        memberDAO.deleteMember(id);
+        membersDAO.deleteMember(id);
     }
 
     // Issue and return books
     public void issueBook(int memberId, int bookId, Date issueDate, Date dueDate) {
-        transactionDAO.issueBook(memberId, bookId, issueDate, dueDate);
+        transactionsDAO.issueBook(memberId, bookId, issueDate, dueDate);
     }
 
     public void returnBook(int transactionId, Date returnDate) {
-        transactionDAO.returnBook(transactionId, returnDate);
+        transactionsDAO.returnBook(transactionId, returnDate);
     }
 
     // Track due dates
     public void trackDueDate() {
-        transactionDAO.trackDueDate();
+        transactionsDAO.trackDueDate();
     }
 }
